37f8b312e8635c7e02a1ade60614e694caeb58ad
akazhuo <akazhuo@qq.com>
2026-05-21 18:01:18 +0800

    chore:

--- src/routes/brand.ts
+++ src/routes/brand.ts
@@ -16,8 +16,24 @@ router.get('/', async (req: Request, res: Response) => {
 
 router.get('/search', async (req: Request, res: Response) => {
   const collection = await db.collection('brands')
+  const results = await collection
+    .aggregate([
+      // 1. 先过滤出你想要搜索的品牌（例如品牌名为 "Nike"）
+      {
+        $match: { name: req.query.name }
+      },
+      {
+        $lookup: {
+          from: 'products',
+          localField: '_id',
+          foreignField: 'brand_ids',
+          as: 'products'
+        }
+      }
+    ])
+    .toArray()
   const query = req.query
-  const results = await collection.find(query).toArray()
+  //   const results = await collection.find(query).toArray()
   res.json({
     code: 0,
     success: true,

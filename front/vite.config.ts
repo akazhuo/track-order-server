1d4a8ffe37c5e5d91ad12740a1338a913ac0c727
akazhuo <akazhuo@qq.com>
2026-05-22 09:36:49 +0800

    build(front): 兼容 chrome76

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    legacy({
+      targets: ["chrome 76", "> 1%", "last 2 versions", "not dead"],
+    }),
+  ],
+});

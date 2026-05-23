1457f1ab57b07a030eb6ed28f0b346f20970fe11
akazhuo <akazhuo@qq.com>
2026-05-23 17:59:57 +0800

    feat: 品牌新增接口

--- src/routes/brand.ts
+++ src/routes/brand.ts
@@ -25,4 +25,14 @@ router.get('/search', async (req: Request, res: Response) => {
   })
 })
 
+router.post('/add', async (req: Request, res: Response) => {
+  const { product_id, name } = req.body
+  res.status(400).send()
+  // const products = db.collection('products')
+  // products.updateOne(
+  //   { _id: ObjectId('targetProductId') }, // 1. 筛选出要更新的目标产品
+  //   { $push: { brand_ids: ObjectId('newBrandId') } } // 2. 将新品牌ID推入 brand_ids 数组
+  // )
+})
+
 export default router

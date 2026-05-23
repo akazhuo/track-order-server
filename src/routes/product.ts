b0a48960d7fa7775557f0f1989cc8d4dfbde74d5
akazhuo <akazhuo@qq.com>
2026-05-23 13:46:55 +0800

    chore: 搜索 product 时关联品牌集合数据

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -5,9 +5,23 @@ const router = express.Router()
 
 router.get('/', async (req: Request, res: Response) => {
   const collection = await db.collection('products')
-  const results = await collection.find({}).toArray()
+  const results = await collection
+    .aggregate([
+      // // 1. 先过滤出你想要搜索的商品名
+      // {
+      //   $match: { name: req.query.name }
+      // },
+      {
+        $lookup: {
+          from: 'brands',
+          localField: 'brand_ids',
+          foreignField: '_id',
+          as: 'brands'
+        }
+      }
+    ])
+    .toArray()
   res.json({
-    code: 0,
     success: true,
     IsOK: true,
     Extra: JSON.stringify(results)

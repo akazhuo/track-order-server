dd970ec856132ead53fa8b8e8b4cf2d8cc8a58d3
akazhuo <akazhuo@qq.com>
2026-05-27 15:02:48 +0800

    chore: 修改上传中间件

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express'
+import { Request } from 'express'
 import multer from 'multer'
 import path from 'path'
 import fs from 'fs'
@@ -7,17 +7,19 @@ import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 // 获取 __dirname 的 ESM 写法
 const __dirname = dirname(fileURLToPath(import.meta.url))
+// 限制单文件最大 50MB (考虑到视频)
+export const LIMIT_FILE_SIZE = 50 * 1024 * 1024
 
 // 确保 uploads 目录存在
-const uploadDir = path.resolve(__dirname, './uploads')
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true })
+export const UPLOAD_DIR = path.resolve(__dirname, process.env.UPLOAD_DIR as string)
+if (!fs.existsSync(UPLOAD_DIR)) {
+  fs.mkdirSync(UPLOAD_DIR, { recursive: true })
 }
 
 // 配置 multer 的磁盘存储引擎
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir) // 文件保存的文件夹
+    cb(null, UPLOAD_DIR) // 文件保存的文件夹
   },
   filename: (req, file, cb) => {
     // 防止文件名重复：时间戳 + 原始文件后缀
@@ -27,25 +29,29 @@ const storage = multer.diskStorage({
 })
 
 // 文件过滤（只允许上传图片）
-const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-  const allowedTypes = [
-    'image/jpeg',
-    'image/png',
-    'image/gif',
-    'image/webp',
-    'video/mp4',
-    'video/quicktime'
-  ]
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true)
-  } else {
-    cb(new Error('不支持的文件类型，请上传图片或 MP4/MOV 视频！'))
+export const fileFilter = {
+  video: () => {},
+  img: () => {},
+  videoImg: (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+    const allowedTypes = [
+      'image/jpeg',
+      'image/png',
+      'image/gif',
+      'image/webp',
+      'video/mp4',
+      'video/quicktime'
+    ]
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(new Error('不支持的文件类型，请上传图片或 MP4/MOV 视频！'))
+    }
   }
 }
 
 // 实例化 multer
 export default multer({
   storage: storage,
-  fileFilter: fileFilter,
-  limits: { fileSize: 50 * 1024 * 1024 } // 限制单文件最大 50MB (考虑到视频)
+  fileFilter: fileFilter.videoImg,
+  limits: { fileSize: LIMIT_FILE_SIZE }
 })
